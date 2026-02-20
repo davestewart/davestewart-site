d2688f44cc38f8fcaf206b9e78e9e1e5f6580dba
Dave Stewart <dev@davestewart.co.uk>
2026-02-20 22:07:46 +0000

    feat: tweak table display on mobile

--- app/components/content/ProseTable.vue
+++ app/components/content/ProseTable.vue
@@ -8,16 +8,31 @@
 
 <style lang="scss">
 .table-wrapper {
+  margin: 2rem 0;
   width: 100%;
   overflow-x: auto;
   -webkit-overflow-scrolling: touch;
 
   @include lg {
     margin: 2rem 0 2rem 1rem;
+  }
 
-    table {
+  table {
+    @include md-down {
+      min-width: 1000px; // factor in larger font size
+      margin: 1rem 0;
+    }
+
+    @include md-up {
+      min-width: 840px; // normal content width
+    }
+
+    @include lg {
+      margin: 0;
       max-width: calc(100% - 1rem);
+      min-width: unset;
     }
   }
+
 }
 </style>
