74ee3b6ae411cccd6fa397ab96b36361472b0a13
Dave Stewart <dev@davestewart.co.uk>
2026-02-20 12:43:05 +0000

    chore: minor updates

---
+++ app/components/content/ScrollH.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="scroll-h">
+    <slot />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.scroll-h {
+  display: flex;
+  width: 100%;
+  margin: 2rem 0;
+  overflow-x: scroll;
+
+  > .table-wrapper > table,
+  > * {
+    margin: 0 !important;
+    white-space: nowrap;
+  }
+}
+</style>
