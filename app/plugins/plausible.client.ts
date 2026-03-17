d9d4564e96b29073b6adfc8c256af432df1f5c4c
Dave Stewart <dev@davestewart.co.uk>
2026-03-17 16:57:17 +0000

    chore: add analytics to davestewart.io

--- app/plugins/plausible.client.ts
+++ app/plugins/plausible.client.ts
@@ -10,4 +10,17 @@ export default defineNuxtPlugin(() => {
       }],
     })
   }
+  if (window.location.host.includes('davestewart.io')) {
+    useHead({
+      script: [
+        {
+          src: 'https://plausible.io/js/pa-Wvehpt8QAWyNFL6Rw9y67.js',
+          async: true,
+        },
+        {
+          innerHTML: 'window.plausible=window.plausible||function(){(plausible.q=plausible.q||[]).push(arguments)},plausible.init=plausible.init||function(i){plausible.o=i||{}}; plausible.init()',
+        },
+      ],
+    })
+  }
 })
