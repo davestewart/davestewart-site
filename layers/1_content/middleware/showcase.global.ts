f33de29227e6bface76877b47aa28aa6e3c32d9a
Dave Stewart <dev@davestewart.co.uk>
2026-03-17 20:55:03 +0000

    feat: tighten-up showcase redirection

--- layers/1_content/middleware/showcase.global.ts
+++ layers/1_content/middleware/showcase.global.ts
@@ -1,20 +1,28 @@
 export default defineNuxtRouteMiddleware(async () => {
+  const redirect = () => navigateTo('https://davestewart.co.uk', { external: true })
   const headers = useRequestHeaders()
   const host = headers['host'] ?? ''
+
+  // always redirect from top-level davestewart.io
+  if (host === 'davestewart.io') {
+    return redirect()
+  }
+
+  // otherwise, check for subdomain and validate it
+  // this only works on davestewart.io and localhost
   const match = host.match(/^([^.]+)\.(davestewart\.io|localhost)/)
   const showcase = match?.[1] ?? null
-
   if (showcase) {
     const data = await $fetch('/api/content/page', {
       query: { path: `/${showcase}/` },
-    }) ?? null
+    })
 
+    // if subdomain but no data, redirect
     if (!data) {
-      navigateTo('https://davestewart.co.uk', {
-        external: true,
-      })
+      return redirect()
     }
   }
 
+  // finally, set showcase or null state
   useState('showcase', () => showcase)
 })
